9f353036612a6d3ec411df309741fac9619d854d
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-21 11:58:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Your code here
-
+const employees = []
 
 function createEmployeeRecord(array){
-    const date = new Date().toLocaleDateString('en-CA')
-    const today = new Date()
-    let time = today.getHours()
-    time = (time +"00")
-    // `${date} ${time}`
+    // const date = new Date().toLocaleDateString('en-CA')
+    // const today = new Date()
+    // let time = today.getHours()
+    // time = (time +"00")
+    // // `${date} ${time}`
+    
     const employee = {
     firstName:  array[0],
     familyName: array[1],
@@ -15,5 +16,22 @@ function createEmployeeRecord(array){
     timeInEvents: [],
     timeOutEvents: []
     }
+employees.push(employee)
 return employee
+}
+
+function createEmployeeRecords(arrayOfArrays){
+
+return arrayOfArrays.map( employeeArray => createEmployeeRecord(employeeArray))
+}
+
+function createTimeInEvent(employeeObject, dateStamp) {
+    
+    let stamp = dateStamp.split(" ")
+
+    employeeObject.timeInEvents = {
+        type: "TimeIn",
+        hour: `${stamp[1]}`, 
+        date: `${stamp[0]}`
+    }
 }
