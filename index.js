f349d0f0f13b2cce9a4fe529aa10040afe0c130e
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-21 11:25:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+
+function createEmployeeRecord(array){
+    const date = new Date().toLocaleDateString('en-CA')
+    const today = new Date()
+    let time = today.getHours()
+    time = (time +"00")
+    // `${date} ${time}`
+    const employee = {
+    firstName:  array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+return employee
+}
